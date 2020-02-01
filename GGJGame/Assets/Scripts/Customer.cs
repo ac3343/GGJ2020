fab08dcb0cbae8e6bd042fe568956d510750005a
WSquared88 <westleywaligora@gmail.com>
2020-02-01 13:28:27 -0500

    Added a customer manager that can debug spawn customers by pressing S Made a customer class that has a basic order that can be fulfilled and who eventually despawns if they wait too long or are satisfied

---
+++ GGJGame/Assets/Scripts/Customer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public struct CustomerOrder
+    {
+        public ResourceNode m_Resource;
+        public LocationNode m_Destination;
+        public float m_OrderDuration;
+        public float m_OrderFulfillmentDuration;
+        public int m_Reward;
+
+        public bool IsValid()
+        {
+            //The order fulfillment duration can be zero or less, that just auto completes the order when the player satisfies the conditions
+            return m_Resource && m_Destination && m_OrderDuration > 0 && m_Reward > 0;
+        }
+    }
+
+public class Customer : MonoBehaviour
+{
+    [ReadOnly]
+    [SerializeField]
+    CustomerOrder m_Order;
+    float m_CurrentOrderDuration;
+    float m_CurrentOrderFulfillmentDuration;
+    bool m_OrderSatisfied;
+
+    void Awake()
+    {
+        m_CurrentOrderDuration = 0.0f;
+        m_CurrentOrderFulfillmentDuration = 0.0f;
+        m_OrderSatisfied = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_OrderSatisfied)
+        {
+            m_CurrentOrderDuration -= Time.deltaTime;
+
+            if (m_CurrentOrderDuration <= 0.0f)
+            {
+                Debug.Log("The player took to long to satisfy my order!");
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            m_CurrentOrderFulfillmentDuration -= Time.deltaTime;
+
+            if(m_CurrentOrderFulfillmentDuration <= 0.0f)
+            {
+                Debug.Log("The player fulfilled my order!");
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    public void SetupCustomer(CustomerOrder order)
+    {
+        if(!order.IsValid())
+        {
+            Debug.LogError("The customer was given a bad order when it was set up! The order is " + order);
+        }
+
+        m_Order = order;
+        m_CurrentOrderDuration = m_Order.m_OrderDuration;
+        m_CurrentOrderFulfillmentDuration = m_Order.m_OrderFulfillmentDuration;
+    }
+
+    public void UpdateOrder()
+    {
+        m_OrderSatisfied = GraphSolver.Instance.FindPath(m_Order.m_Resource, m_Order.m_Destination, false);
+    }
+}
---
+++ GGJGame/Assets/Scripts/CustomerManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class determines when a new customer should be made
+public class CustomerManager : Singleton<CustomerManager>
+{
+    [SerializeField]
+    int m_MaxRewardAmount;
+    List<Customer> m_Customers;
+
+    private void Awake()
+    {
+        m_Customers = new List<Customer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Spawn"))
+        {
+            Debug.Log("Spawning a customer");
+            GameObject customer = new GameObject();
+            customer.name = "Customer";
+            Customer customer_comp = customer.AddComponent<Customer>();
+            
+            if(customer_comp)
+            {
+                ResourceNode[] resources = GraphSolver.Instance.Resources;
+                LocationNode[] locations = GraphSolver.Instance.Locations;
+                int random_resource_index = Random.Range(0, resources.Length - 1);
+                int random_location_index = Random.Range(0, resources.Length - 1);
+
+                CustomerOrder order;
+                order.m_Resource = resources[random_resource_index];
+                order.m_Destination = locations[random_location_index];
+                order.m_Reward = Random.Range(1, m_MaxRewardAmount);
+                order.m_OrderDuration = 2.0f;
+                order.m_OrderFulfillmentDuration = 0;
+
+                customer_comp.SetupCustomer(order);
+                m_Customers.Add(customer_comp);
+            }
+        }
+    }
+
+    public void UpdateCustomerOrders()
+    {
+        for(int i = 0;i<m_Customers.Count;i++)
+        {
+            m_Customers[i].UpdateOrder();
+        }
+    }
+}
