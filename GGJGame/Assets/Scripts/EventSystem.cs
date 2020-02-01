fe06d7043f1cfb96d8f222506eda0fdb7de1a72e
WSquared88 <westleywaligora@gmail.com>
2020-02-01 15:56:14 -0500

    Created an EventSystem that can be used to pass events through to different objects without needing to be a singleton or heavily coupled

---
+++ GGJGame/Assets/Scripts/EventSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystem : Singleton<EventSystem>
+{
+    public delegate void PowerConsumerActiveStateChangeEvent();
+    public static event PowerConsumerActiveStateChangeEvent PowerConsumerActiveStateChangeHandler;
+
+    public void OnPowerConsumerActiveStateChange()
+    {
+        PowerConsumerActiveStateChangeHandler();
+    }
+    //In a real game we would probably throw an error if the game ended and we still had people listening to the event
+}
