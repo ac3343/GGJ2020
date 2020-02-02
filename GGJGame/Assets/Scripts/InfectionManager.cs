a3b57a34510fc360dc4be7289b71ee39a3068753
Andrew Cambridge <56008700+ac3343@users.noreply.github.com>
2020-02-02 12:33:06 -0500

    Infection meter created

---
+++ GGJGame/Assets/Scripts/InfectionManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfectionManager : Singleton<InfectionManager>
+{
+    [SerializeField]
+    float m_InfectPercent;
+    NodeInfectionTimer[] m_NodeInfections;
+    Scrollbar m_ScrollbarInfo;
+    
+
+    public override void Awake()
+    {
+        base.Awake();
+        m_NodeInfections = FindObjectsOfType<NodeInfectionTimer>();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ScrollbarInfo = GetComponent<Scrollbar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float infectTotal = 0;
+
+        for(int i = 0; i < m_NodeInfections.Length; i++)
+        {
+            infectTotal += m_NodeInfections[i].InfectionPercent;
+        }
+
+        m_InfectPercent = infectTotal / m_NodeInfections.Length;
+
+        m_ScrollbarInfo.size = m_InfectPercent;
+    }
+}
